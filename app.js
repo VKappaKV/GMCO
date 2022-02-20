4e03daea060177cc3634887cdb04a90696b8007d
Jacopo Mileto <jacopo851@gmail.com>
2022-02-20 18:12:26 +0100

    rb

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 function checkData() {
   const form_data = window.document.myForm;
-  let error;
-  if (form_data.feedback.value == null) {
-    error = "Field is empty";
-    alert(error);
+  if (form_data.Nome.value == "" || form_data.Cognome.value == "") {
+    alert("Riempire i campi Nome e Cognome");
   }
-  alert(
-    form_data.Nome.value +
-      form_data.Cognome.value +
-      " sesso: " +
-      form_data.gender.value +
-      "\n" +
-      form_data.feedback.value
-  );
+}
+
+function checkEmpty() {
+  let valueofId = document.getElementById("myForm").value;
+  if (!valueofId) {
+    alert("empty fields");
+    return false;
+  }
 }
